70a17857b4505c7513a97698ea749230a84fc4a4
EarnNetiya <s6506022630015@email.kmutnb.ac.th>
2023-09-26 09:49:03 +0700

    new

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require('path');
 var bodyParser = require('body-parser');
 
-const base_url = 'http://10.104.15.175';
+const base_url = 'http://localhost:3000';
+app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
